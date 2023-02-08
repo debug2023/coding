fb0206de45756b2f3308568c84cc71df49796101
debug2023 <124577797+debug2023@users.noreply.github.com>
2023-02-08 12:03:26 +0800

    Delete SKM CU03 directory

--- SKM CU03/CU03/06. KERTAS KERJA/BAHAN CU_KPD2042/BAHAN ASTAKONA/astakona_v1/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//DATABASE CONNECTION
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "astakona2";
-$conn=mysqli_connect($servername, $username, $password, $dbname);
-?>
